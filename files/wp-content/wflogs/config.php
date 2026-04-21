b93f60fe2ab7267b4dfb329b7b4509e5fb85ac06
kriangkrai <st651102064101@pcru.ac.th>
2026-04-21 15:29:18 +0700

    Update Wordfence WAF configuration: change status from learning-mode to enabled and disable learning mode grace period

--- files/wp-content/wflogs/config.php
+++ files/wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1777296582;s:7:"authKey";s:64:"H]2*t5E0}O;QX6kWT)^/ThRb8p$D?uF][#f|}$cvMeZ/}?sFZ%81:y6`%+AjKiL<";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1441;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"H]2*t5E0}O;QX6kWT)^/ThRb8p$D?uF][#f|}$cvMeZ/}?sFZ%81:y6`%+AjKiL<";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1441;}
