c8f8edc11af3ec136728289fc8009d732c39f589
kriangkrai <st651102064101@pcru.ac.th>
2026-04-21 21:12:56 +0700

    Add Wordfence Security plugin with WAF user IP range handling
    - Introduced `wfWAFUserIPRange` class for managing IP ranges including CIDR, bracketed, and linear formats.
    - Implemented methods for validating IP ranges and checking if an IP is within a specified range.
    - Added support for expanding IPv6 ranges and sanitizing IP range inputs.
    - Created main plugin file `wordfence.php` with necessary plugin metadata and initialization logic.
    - Included checks for PHP and WordPress version compatibility, displaying admin notices for unsupported versions.

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1777378886;s:7:"authKey";s:64:"tb{r8/$2rwftLXe?lrYo,bEj75U}GhF]>ct?vfF{nT`Yle#:yQGG|N(m}7e;5h2K";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2306;}
