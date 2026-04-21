c33feaf2cd87562766f48810b8e1b99c34eaa36f
kriangkrai <st651102064101@pcru.ac.th>
2026-04-21 15:18:00 +0700

    edit

--- files/wp-content/wflogs/config.php
+++ files/wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1777296582;s:7:"authKey";s:64:"H]2*t5E0}O;QX6kWT)^/ThRb8p$D?uF][#f|}$cvMeZ/}?sFZ%81:y6`%+AjKiL<";s:7:"version";s:5:"1.1.0";}
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1777296582;s:7:"authKey";s:64:"H]2*t5E0}O;QX6kWT)^/ThRb8p$D?uF][#f|}$cvMeZ/}?sFZ%81:y6`%+AjKiL<";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1441;}
